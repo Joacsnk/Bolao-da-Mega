b1665574e04467df40a1f763cfaf50cc70759eeb
Jackson Lobo <Joacsnk@gmail.com>
2026-01-09 12:17:47 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+def gerar_cartela(numero_dezenas, numero_jogos):
+    
+    lista_jogos = []
+    for i in range(numero_jogos):
+        numeros = []
+        for i in range(numero_dezenas):
+            while True:
+                numero_aleatorio = random.randint(1, 60)
+                if numero_aleatorio in numeros:
+                    continue
+                else:
+                    numeros.append(numero_aleatorio)
+                    break
+        numeros.sort()
+        
+        lista_jogos.append(numeros)
+        
+    return lista_jogos
+
+            
+        
+print(gerar_cartela(6, 5))
+
+
+
+    
+    
